6b7ae72da305bdc9d38189f8ca9ffe2fac06525a
paulo4fs <engpaulofrota@gmail.com>
2021-02-10 09:53:10 -0300

    feat: Mutual recursion 6kiu

---
+++ Kotlin/src/sixthKyu/mutualRecursion/MutualRecursion.kt
@@ -0,0 +1,6 @@
+package sixthKyu.mutualRecursion
+
+object MutualRecursion {
+    fun f(n: Int): Int = if (n > 0) (n - m(f(n - 1))) else 1
+    fun m(n: Int): Int = if (n > 0) (n - f(m(n - 1))) else 0
+}
---
+++ Kotlin/src/sixthKyu/pile_of_cubes/ASum.kt
@@ -0,0 +1,10 @@
+package sixthKyu.pile_of_cubes
+
+object ASum {
+
+    fun findNb(m: Long): Long {
+        // your code
+
+        return 0L
+    }
+}
